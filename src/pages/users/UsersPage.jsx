216f363c831a0f5fe6b39c8a78da5855269cfcd9
Elvis <elvinasu@gmail.com>
2021-12-17 16:27:21 +0200

    useriai atvaizudojami fronte

--- src/pages/users/UsersPage.jsx
+++ src/pages/users/UsersPage.jsx
@@ -1,13 +1,14 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import Navbar from "../../component/Navbar/Navbar";
 import css from './UserPage.module.css'
 import Axios from "axios";
 
 
 function UsersPage(){
+    const [usersList, setUsersList]= useState([])
     useEffect(()=> {
         Axios.get('http://localhost:5000/getUsers').then((response)=>{
-            console.log(response.data)
+            setUsersList(response.data)
         })
     }, [])
     return(
@@ -17,7 +18,10 @@ function UsersPage(){
 
             <h1 className={css.h1}>Visi Vartotojai</h1>
 
+                {usersList.map((value)=>{
+                    return <li>name:{value.name}</li>
 
+                })}
             </div>
 
         </div>
