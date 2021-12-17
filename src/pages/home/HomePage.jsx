e9817c5f63d73b7ea7dbafe984f3278261fcf603
Elvis <elvinasu@gmail.com>
2021-12-17 09:29:31 +0200

    homePage

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,9 +1,10 @@
 import css from "./HomePage.module.css";
+import User from '../../assets/images/users7.png'
 
 function HomePage(){
     return(
         <div>
-
+        <img className={css.img} src={User}/>
 
 
 
